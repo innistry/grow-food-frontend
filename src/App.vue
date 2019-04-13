b5b7337e46322032cdc92b609993aa0828e7b7a6
naryshkin <i.naryshkin@telemetron.net>
2019-04-13 21:05:12 +0300

    init vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <input type="text" />
+
+    <b-alert show>Default Alert</b-alert>
+
+    <div>
+      <b-button @click="acha" variant="outline-primary">Button</b-button>
+    </div>
+
+    <div>
+      {{ axe }}
+    </div>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                'axe': ''
+            }
+        },
+        methods: {
+            async acha() {
+                try {
+                    this.axe = await axios.get('http://127.0.0.1:8000/api')
+
+                } catch (error) {
+                    return false
+                }
+            }
+        }
+    }
+</script>
